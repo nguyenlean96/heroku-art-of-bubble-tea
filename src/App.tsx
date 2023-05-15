82c3f58125e669265d332a16378f8eb27adf05ff
btran <nguyenbao1003@outlook.com>
2023-05-14 21:52:06 -0400

    Add reset list button

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
-import { Fragment } from 'react';
+import { MDBContainer, MDBRow, MDBCol, MDBBtn } from 'mdb-react-ui-kit';
 import { KF_TEA_LIST } from '../src/constants';
 import { shuffleArray } from '../src/utils';
+import { useReducer } from 'react';
 function App() {
+  const [_, forceUpdate] = useReducer((x) => x + 1, 0);
   return (
-    <Fragment>
-      <h1>Kung Fu tea</h1>
-      <ol>
-        {shuffleArray(KF_TEA_LIST).map((tea) => (
-          <li key={tea.toUpperCase()}>{tea}</li>
-        ))}
-      </ol>
-    </Fragment>
+    <MDBContainer>
+      <MDBRow className='p-3'>
+        <MDBCol>
+          <h1>Kung Fu tea</h1>
+        </MDBCol>
+        <MDBCol>
+          <MDBBtn onClick={forceUpdate}>Reset list</MDBBtn>
+        </MDBCol>
+      </MDBRow>
+      <MDBRow>
+        <MDBCol>
+          <ol>
+            {shuffleArray(KF_TEA_LIST).map((tea) => (
+              <li key={tea.toUpperCase()}>{tea}</li>
+            ))}
+          </ol>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   );
 }
 
