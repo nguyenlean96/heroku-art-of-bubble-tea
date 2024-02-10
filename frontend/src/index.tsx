4b2134a05eed674258147ed72119109d04d76f7c
Le An Nguyen <65287357+nguyenlean96@users.noreply.github.com>
2024-02-10 09:39:10 -0500

    Implemented React Router (#2)
    * Files removed due to structure changes made to implement routing
    
    * Implemented react-router-dom
    
    * Landing page created
    
    * Placeholder for recipes list
    
    * App title updated

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,25 @@
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import LandingPage from './routes/page';
+import RecipesPage from './routes/recipes/page';
+import * as ReactDOM from 'react-dom/client';
+import  {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />
+  },
+  {
+    path: "/recipes",
+    element: <RecipesPage />
+  }
+]);
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 ).render(
-  <App />
+  <RouterProvider router={router} />
 );
---
+++ frontend/src/routes/recipes/page.jsx
@@ -0,0 +1,11 @@
+
+export default function RecipesPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Recipes</h1>
+      <div>
+        To be implemented...
+      </div>
+    </div>
+  );
+}
