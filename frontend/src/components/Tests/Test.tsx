a15ee362043b3830f05a80ffd727037129cd7bc6
Le An Nguyen <65287357+nguyenlean96@users.noreply.github.com>
2024-05-04 20:45:02 -0400

    Reconstruct front-end recipe list layout (#10)
    * Files removed due to structure changes made to implement routing
    
    * Implemented react-router-dom
    
    * Landing page created
    
    * Placeholder for recipes list
    
    * App title updated
    
    * Refactor codebase (#4)
    
    * organize file structure
    
    * refactor codebase
    
    * Implemented React Router (#2)
    
    * Files removed due to structure changes made to implement routing
    
    * Implemented react-router-dom
    
    * Landing page created
    
    * Placeholder for recipes list
    
    * App title updated
    
    * Refactor codebase (#4)
    
    * organize file structure
    
    * refactor codebase
    
    * Implement client routing
    
    * Now makes it an SPA
    
    * Implemented React Router (#2)
    
    * Files removed due to structure changes made to implement routing
    
    * Implemented react-router-dom
    
    * Landing page created
    
    * Placeholder for recipes list
    
    * App title updated
    
    * Refactor codebase (#4)
    
    * organize file structure
    
    * refactor codebase
    
    * Reverted to its previous state
    
    * Fixed typo
    
    * Exclude parts of the frontend for local developing purposes only
    
    * Bring tailwind back
    
    * Removed unused part
    
    * Fill up the gap of the original react structure
    
    * Put some fields optional to bypass ts compiler
    
    * Frontend structure refactored
    
    * Test list components added
    
    * fixed tailwindcss
    
    * sample data type
    
    * Rename data hook since real data is usable
    
    * Reconstruct layout
    
    * Fixed sticky text with ingredients having more than 2 fields
    
    ---------
    
    Co-authored-by: Bao Tran <nguyenbao1003@hotmail.com>

--- frontend/src/components/Tests/Test.tsx
+++ frontend/src/components/Tests/Test.tsx
@@ -1,12 +1,19 @@
 import { useState } from 'react';
-import useFakeData from '../../hooks/useFakeData';
+import useKungfuData from '../../hooks/useKungfuData';
+import { KFTea, Recipe } from '../../types/kf';
 
 export default function Test() {
 	const [numberOfQuestions, setNumberOfQuestions] = useState<number>(10);
 	const [isShuffled, setIsShuffled] = useState<boolean>(false);
 	const [currentPage, setCurrentPage] = useState(1);
 
-	const { recipes } = useFakeData({
+	const {
+		recipes,
+		types,
+	}: {
+		recipes: KFTea[];
+		types: string[];
+	} = useKungfuData({
 		recipesCount: numberOfQuestions,
 		shuffled: isShuffled,
 	});
@@ -17,10 +24,10 @@ export default function Test() {
 	};
 
 	const startIndex = (currentPage - 1) * 10; // Pagination set to 10 by default
-	const currentItems = recipes.slice(startIndex, startIndex + 10); // Pagination set to 10 by default
+	const currentItems: KFTea[] = recipes.slice(startIndex, startIndex + 10); // Pagination set to 10 by default
 	return (
-		<div className='flex items-start'>
-			<div className='flex flex-col items-center'>
+		<div className='flex items-start w-full'>
+			<div className='flex flex-col items-center w-full'>
 				<div className='flex items-center justify-end gap-3 p-3 leading-none'>
 					<div className=''>
 						<label className='inline-flex items-center mb-5 cursor-pointer text-sm font-medium text-gray-800 dark:text-white'>
@@ -60,14 +67,14 @@ export default function Test() {
 					</div>
 				</div>
 
-				<div className='flex flex-col gap-3 md:gap-4 px-5 md:px-10'>
+				<div className='w-full flex flex-col gap-3 md:gap-4 px-5 md:px-10'>
 					{currentItems &&
-						currentItems.map((answer: any, index: number) => (
-							<div key={answer.id}>
-								<div className='bg-white ring-1 ring-black ring-opacity-10 rounded-lg shadow-lg'>
-									<div className='border p-2'>{JSON.stringify(answer)}</div>
-									<div className=''></div>
-								</div>
+						currentItems.map((answer: KFTea, index: number) => (
+							<div key={index}>
+								<AnswerDisplay
+									answer={answer}
+									types={types}
+								/>
 							</div>
 						))}
 				</div>
@@ -88,3 +95,109 @@ export default function Test() {
 		</div>
 	);
 }
+
+const AnswerDisplay = ({ answer, types }: { answer: KFTea; types: Recipe[] }) => {
+	const { name, availability, note, ...others } = answer;
+	const getTypes = (): string[] => {
+		// Get all the types from fields: syrup, tea, water, milk, honey
+		const types: string[] = [];
+		Object.keys(others).forEach((key: string) => {
+			if (others[key]) {
+				others[key].forEach((props: any) => {
+					if (!types.includes(props?.type)) {
+						types.push(props?.type);
+					}
+				});
+
+				// break the loop if all types are found
+				if (types.length > 0) {
+					return types;
+				}
+			}
+		});
+
+		return types;
+	};
+
+	const foundTypes: string[] = getTypes();
+
+	return (
+		<div className='relative bg-white ring-1 ring-black ring-opacity-10 rounded-lg shadow-lg px-3 md:px-5 py-2 md:py-4'>
+			<div className='absolute top-2 right-3'>
+				<div className='flex items-center gap-2'>
+					<>
+						{availability ? (
+							<span className='rounded-lg bg-lime-700 text-white text-center px-2 py-1 text-xs'>
+								{'In stock'}
+							</span>
+						) : (
+							<span className='rounded-lg bg-gray-200 text-gray-800 text-center px-2 py-1 text-xs'>
+								{'Out of stock'}
+							</span>
+						)}
+					</>
+					<>
+						{foundTypes.map((type: string, index: number) => (
+							<span
+								key={index}
+								className='bg-blue-500 text-white text-xs px-2 py-1 rounded-lg'
+							>
+								{types[parseInt(type)]}
+							</span>
+						))}
+					</>
+				</div>
+			</div>
+			<h3 className={'font-lg font-semibold pb-1' + (availability && ' border-b mb-3')}>{answer?.name}</h3>
+			<ul className='flex flex-col lg:flex-row w-full justify-evenly gap-x-3'>
+				{foundTypes.map((type: string, index: number) => (
+					<li
+						key={index}
+						className='w-full'
+					>
+						<div className='ring-2 ring-black/10 w-full'>
+							<div className='bg-blue-100 text-gray-600 p-2'>{types[type]}</div>
+							<div className='px-3 lg:px-4 py-3'>
+								{others &&
+									Object.keys(others).length > 0 &&
+									Object.keys(others).map((key: string, index: number) => (
+										<div
+											key={index}
+											className='flex flex-wrap gap-2 w-full mb-1'
+										>
+											{others[key] &&
+												others[key] !== null &&
+												others[key].filter(
+													(item: any) => item.type === parseInt(type)
+												)[0] && (
+													<>
+														<span className='bg-indigo-500 rounded px-2 text-white'>{`${key}:`}</span>
+														<span>
+															{Object.entries(
+																others[key].filter(
+																	(item: any) => item.type === parseInt(type)
+																)[0]
+															).map(
+																([key, value]: [string, any], index: number) =>
+																	key !== 'type' && (
+																		<span
+																			key={index}
+																			className='text-sm'
+																		>
+																			{`${value} `}
+																		</span>
+																	)
+															)}
+														</span>
+													</>
+												)}
+										</div>
+									))}
+							</div>
+						</div>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
